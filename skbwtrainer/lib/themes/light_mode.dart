a0973054b0c5c6eeb52961080cd7d18ba24480bc
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2024-12-23 16:02:44 +0530

    theme

---
+++ skbwtrainer/lib/themes/light_mode.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    primary: Color(0xFFFF9900), // Lime green as the primary color
+    onPrimary: Color(0xFF000000), // Black for text/icons on primary
+    secondary: Color(0xFFFFFFFF), // White as the secondary color
+    onSecondary: Color(0xFF000000), // Black for text/icons on background
+    tertiary: Color(0xFF363636),
+    onTertiary: Color(0xFFFFFFFF),
+    surface: Color(0xFFF0F0F0), // White for surface elements (e.g., card backgrounds)
+    onSurface: Color(0xFF000000), // Black for text/icons on surface
+    error: Color(0xFFB00020), // Red for errors
+    onError: Color(0xFFFFFFFF), // White for text/icons on errors
+  ),
+  textTheme: ThemeData.light().textTheme.apply(
+    bodyColor: Colors.grey.shade800,
+    displayColor: Colors.black,
+  ),
+);
