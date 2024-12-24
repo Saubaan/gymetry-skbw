73d10c3fe253ac44f55062aade6dc039973bb196
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2024-12-24 23:36:02 +0530

    Changes in main.dart

--- skbwtrainer/lib/main.dart
+++ skbwtrainer/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+import 'config/firebase_options.dart';
 import 'gymetry.dart';
 
 void main() async {
