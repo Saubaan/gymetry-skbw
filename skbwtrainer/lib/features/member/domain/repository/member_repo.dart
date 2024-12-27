c6e61807c656f55722ec7d73f829aacd5b4974c4
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2024-12-28 01:02:21 +0530

    Created member_repo.dart

---
+++ skbwtrainer/lib/features/member/domain/repository/member_repo.dart
@@ -0,0 +1,29 @@
+import 'package:skbwtrainer/features/member/domain/entities/attendance.dart';
+import 'package:skbwtrainer/features/member/domain/entities/member.dart';
+import 'package:skbwtrainer/features/member/domain/entities/pending_member.dart';
+
+abstract class MemberRepo {
+  //approve and create member
+  Future<void> approveAndCreateMember(PendingMember pendingMember);
+
+  //get list of all pending members
+  Future<List<PendingMember>> getPendingMembers();
+
+  //delete pending member
+  Future<void> deletePendingMember(String pendingMemberId);
+
+  //get list of all members
+  Future<List<Member>> getMembers();
+
+  //delete member
+  Future<void> deleteMember(String memberId);
+
+  // fetch pending member by id
+  Future<PendingMember> getPendingMemberById(String pendingMemberId);
+
+  // get member attendance by id
+  Future<List<Attendance>> getMemberAttendanceById(String memberId);
+
+  // fetch member by id
+  Future<Member> getMemberById(String memberId);
+}
