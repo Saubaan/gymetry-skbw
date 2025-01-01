2d95abc4863f41785782101ec86ac7f77cdae87a
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-01 18:20:57 +0530

    Created attendance_states.dart

---
+++ skbwtrainer/lib/features/member/presentation/cubits/attendance_states.dart
@@ -0,0 +1,17 @@
+import 'package:skbwtrainer/features/member/domain/entities/attendance.dart';
+
+abstract class AttendanceState {}
+
+class AttendanceInitial extends AttendanceState {}
+
+class AttendanceLoading extends AttendanceState {}
+
+class AttendanceLoaded extends AttendanceState {
+  final List<Attendance> attendance;
+  AttendanceLoaded(this.attendance);
+}
+
+class AttendanceError extends AttendanceState {
+  final String message;
+  AttendanceError(this.message);
+}
