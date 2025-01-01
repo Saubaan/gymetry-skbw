7b9f114c4de04ab02349b59221e92167fd39c508
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-01 16:23:15 +0530

    Implemented update member method.

--- skbwtrainer/lib/features/auth/data/firebase_auth_repo.dart
+++ skbwtrainer/lib/features/auth/data/firebase_auth_repo.dart
@@ -14,6 +14,8 @@ class FirebaseAuthRepo implements AuthRepo {
         password: password,
       );
 
+
+
       AppUser user = AppUser(
         uid: userCredential.user!.uid,
         email: email,
