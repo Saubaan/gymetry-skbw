d2f257f2ab8cb057bbac983571a16405cba419bf
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-01 18:21:16 +0530

    Created calendar_bloc.dart

---
+++ skbwtrainer/lib/features/member/presentation/components/calendar_bloc.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skbwtrainer/features/member/data/firebase_member_repo.dart';
+import 'package:skbwtrainer/features/member/domain/repository/member_repo.dart';
+import 'package:skbwtrainer/features/member/presentation/components/calendar.dart';
+import 'package:skbwtrainer/features/member/presentation/cubits/attendance_cubit.dart';
+import 'package:skbwtrainer/features/member/presentation/cubits/attendance_states.dart';
+
+class CalendarBloc extends StatelessWidget {
+  final String id;
+  final DateTime startDate;
+  final DateTime endDate;
+  final DateTime focusedDay;
+  final MemberRepo memberRepo = FireBaseMemberRepo();
+
+  CalendarBloc(
+      {super.key,
+      required this.startDate,
+      required this.endDate,
+      required this.focusedDay, required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+    return BlocProvider(
+      create: (context) => AttendanceCubit(memberRepo: memberRepo)..getAttendance(id),
+      child: Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: theme.onSurface.withAlpha(20),
+          border: Border.all(
+            color: theme.onSurface.withAlpha(50),
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: BlocConsumer<AttendanceCubit, AttendanceState>(
+            builder: (context, state) {
+          if (state is AttendanceLoaded) {
+            final presentDates = state.attendance.map((e) => e.date).toList();
+            return AttendanceCalendar(
+                startDate: startDate,
+                endDate: endDate,
+                presentDates: presentDates,
+                focusedDay: focusedDay);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }, listener: (context, state) {
+          if (state is AttendanceError) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.message),
+              ),
+            );
+          }
+        }),
+      ),
+    );
+  }
+}
