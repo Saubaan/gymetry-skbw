6682336c66d9cea15d2df88ab61e91e94e7d200c
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-01 18:20:47 +0530

    Created attendance_cubit.dart

---
+++ skbwtrainer/lib/features/member/presentation/cubits/attendance_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skbwtrainer/features/member/domain/entities/attendance.dart';
+import 'package:skbwtrainer/features/member/domain/repository/member_repo.dart';
+import 'attendance_states.dart';
+
+class AttendanceCubit extends Cubit<AttendanceState> {
+  final MemberRepo memberRepo;
+
+  AttendanceCubit({required this.memberRepo}) : super(AttendanceInitial());
+
+
+
+  Future<void> getAttendance(String id) async {
+    emit(AttendanceLoading());
+    try {
+      final attendance = await memberRepo.getMemberAttendanceById(id);
+      emit(AttendanceLoaded(attendance));
+    } on Exception catch (e) {
+      emit(AttendanceError(e.toString()));
+      List<Attendance> attendance = [];
+      emit(AttendanceLoaded(attendance));
+    }
+  }
+}
