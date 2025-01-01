15d5be3f672736d7f3eeddb8febcae519f841db7
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-01 18:22:48 +0530

    Added to member_cubit.dart

--- skbwtrainer/lib/features/member/presentation/cubits/member_cubit.dart
+++ skbwtrainer/lib/features/member/presentation/cubits/member_cubit.dart
@@ -1,17 +1,74 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skbwtrainer/features/member/domain/entities/member.dart';
+import 'package:skbwtrainer/features/member/domain/entities/pending_member.dart';
 import 'package:skbwtrainer/features/member/domain/repository/member_repo.dart';
 import 'member_states.dart';
 
 class MemberCubit extends Cubit<MemberState> {
   final MemberRepo memberRepo;
 
-  MemberCubit(this.memberRepo) : super(MemberInitial());
+  MemberCubit({required this.memberRepo}) : super(MemberInitial());
 
   Future<void> getMembers() async {
     emit(MemberLoading());
     try {
       final members = await memberRepo.getMembers();
-      emit(MemberLoaded(members));
+      emit(MembersLoaded(members));
+    } on Exception catch (e) {
+      emit(MemberError(e.toString()));
+      emit(MemberInitial());
+    }
+  }
+
+  Future<void> getMember(String id) async {
+    emit(MemberLoading());
+    try {
+      final member = await memberRepo.getMemberById(id);
+      emit(MemberLoaded(member));
+    } on Exception catch (e) {
+      emit(MemberError(e.toString()));
+      emit(MemberInitial());
+    }
+  }
+
+  Future<void> getPendingMembers() async {
+    emit(MemberLoading());
+    try {
+      final members = await memberRepo.getPendingMembers();
+      emit(PendingMembersLoaded(members));
+    } on Exception catch (e) {
+      emit(MemberError(e.toString()));
+      emit(MemberInitial());
+    }
+  }
+
+  Future<void> approveMember(PendingMember pendingMember) async {
+    emit(MemberLoading());
+    try {
+      await memberRepo.approveAndCreateMember(pendingMember);
+      emit(MemberInitial());
+    } on Exception catch (e) {
+      emit(MemberError(e.toString()));
+      emit(MemberInitial());
+    }
+  }
+
+  Future<void> deleteMember(String id) async {
+    emit(MemberLoading());
+    try {
+      await memberRepo.deleteMember(id);
+      emit(MemberInitial());
+    } on Exception catch (e) {
+      emit(MemberError(e.toString()));
+      emit(MemberInitial());
+    }
+  }
+
+  Future<void> updateMember(Member member) async {
+    emit(MemberLoading());
+    try {
+      await memberRepo.updateMemberById(member);
+      emit(MemberInitial());
     } on Exception catch (e) {
       emit(MemberError(e.toString()));
       emit(MemberInitial());
