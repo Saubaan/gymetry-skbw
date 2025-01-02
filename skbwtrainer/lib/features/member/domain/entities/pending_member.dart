2df933c6a363b00112440e2d4e8559209a4e3a83
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-02 23:16:16 +0530

    Removed unnecessary Inheritance.

--- skbwtrainer/lib/features/member/domain/entities/pending_member.dart
+++ skbwtrainer/lib/features/member/domain/entities/pending_member.dart
@@ -1,59 +1,40 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'member.dart';
-
-class PendingMember extends Member {
+class PendingMember {
+  final String name;
+  final String email;
+  final String phone;
+  final DateTime expiryDate;
   final String password;
+  final DateTime createdAt;
 
   PendingMember({
-    required super.uid,
-    required super.name,
-    required super.email,
-    required super.phone,
-    required super.expiryDate,
-    required super.isPaused,
-    required super.pauseStartDate,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.expiryDate,
     required this.password,
-    required super.createdAt,
+    required this.createdAt,
   });
 
-  @override
   Map<String, dynamic> toJson() {
     return {
-      'uid': uid,
       'password': password,
       'name': name,
       'email': email,
       'phone': phone,
       'expiryDate': Timestamp.fromDate(expiryDate),
-      'isPaused': isPaused,
-      'pauseStartDate': Timestamp.fromDate(pauseStartDate),
+      'createdAt': Timestamp.fromDate(createdAt),
     };
-  }
-
-  Member toMember() {
-    return Member(
-      uid: uid,
-      name: name,
-      email: email,
-      phone: phone,
-      expiryDate: expiryDate,
-      isPaused: isPaused,
-      pauseStartDate: pauseStartDate,
-      createdAt: createdAt,
-    );
   }
 
   factory PendingMember.fromJson(Map<String, dynamic> json) {
     return PendingMember(
-      uid: json['uid'],
       password: json['password'],
       name: json['name'],
       email: json['email'],
       phone: json['phone'],
       expiryDate: json['expiryDate'].toDate(),
-      isPaused: json['isPaused'],
-      pauseStartDate: json['pauseStartDate'].toDate(),
       createdAt: json['createdAt'].toDate(),
     );
   }
