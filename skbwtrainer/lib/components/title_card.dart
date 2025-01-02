cfdd3a7029f0f4ea304a50b0ce36a470922b028e
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-02 13:59:30 +0530

    Created title_card.dart.

---
+++ skbwtrainer/lib/components/title_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:skbwtrainer/themes/app_font.dart';
+
+class TitleCard extends StatelessWidget {
+  final String? title;
+  final List<Widget> children;
+  final MainAxisAlignment titleAlign;
+  final CrossAxisAlignment childrenAlign;
+
+  const TitleCard({
+    super.key,
+    this.title,
+    required this.children,
+    this.titleAlign = MainAxisAlignment.start,
+    this.childrenAlign = CrossAxisAlignment.center,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double sWidth = MediaQuery.of(context).size.width;
+    final theme = Theme.of(context).colorScheme;
+    List<Widget> columnChildren = [];
+    if (title != null) {
+      columnChildren.add(
+        Row(
+          mainAxisAlignment: titleAlign,
+          children: [
+            Text(
+              title!,
+              style: TextStyle(
+                fontSize: sWidth / 20,
+                fontFamily: AppFont.logoFont,
+                color: theme.onSecondary,
+              ),
+            ),
+          ],
+        ),
+      );
+      columnChildren.add(const SizedBox(height: 10));
+    }
+    columnChildren.addAll(children);
+    return Container(
+      decoration: BoxDecoration(
+        color: theme.secondary,
+        borderRadius: BorderRadius.circular(sWidth / 30),
+      ),
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: childrenAlign,
+        children: columnChildren,
+      ),
+    );
+  }
+}
