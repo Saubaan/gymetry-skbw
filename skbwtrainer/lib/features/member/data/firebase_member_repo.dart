9e1f4061daec15c25df9d71c8d29bdbbd8da0f3e
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-05 11:38:34 +0530

    Removed redundant functions.

--- skbwtrainer/lib/features/member/data/firebase_member_repo.dart
+++ skbwtrainer/lib/features/member/data/firebase_member_repo.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:skbwtrainer/features/member/domain/entities/attendance.dart';
 import 'package:skbwtrainer/features/member/domain/entities/member.dart';
-import 'package:skbwtrainer/features/member/domain/entities/pending_member.dart';
 import 'package:skbwtrainer/features/member/domain/repository/member_repo.dart';
 
 class FireBaseMemberRepo implements MemberRepo {
@@ -11,62 +10,14 @@ class FireBaseMemberRepo implements MemberRepo {
 
   final CollectionReference membersCollection =
       FirebaseFirestore.instance.collection('members');
-  final CollectionReference pendingMembersCollection =
-      FirebaseFirestore.instance.collection('pendingMembers');
   final CollectionReference attendanceCollection =
       FirebaseFirestore.instance.collection('attendance');
-
-  @override
-  Future<void> approveAndCreateMember(PendingMember pendingMember) async {
-    try {
-      // Create a user account for member
-      UserCredential userCredential =
-          await firebaseAuth.createUserWithEmailAndPassword(
-        email: pendingMember.email,
-        password: pendingMember.password,
-      );
-
-      // Create a member document
-      Member member = Member(
-        uid: userCredential.user!.uid,
-        email: pendingMember.email,
-        name: pendingMember.name,
-        phone: pendingMember.phone,
-        expiryDate: pendingMember.expiryDate,
-        isPaused: false,
-        pauseStartDate: DateTime.now(),
-        createdAt: DateTime.now(),
-      );
-
-      // Delete pending member document from the pendingMembers collection
-      await pendingMembersCollection.doc(pendingMember.email).delete();
-
-      // Add member document to the members collection
-      await membersCollection.doc(member.uid).set(member.toJson());
-    } on FirebaseAuthException catch (e) {
-      throw Exception(e.code);
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
 
   @override
   Future<void> deleteMember(String memberId) async {
     try {
       // Delete member document from the members collection
       await membersCollection.doc(memberId).delete();
-    } on FirebaseAuthException catch (e) {
-      throw Exception(e.code);
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  @override
-  Future<void> deletePendingMember(String pendingMemberId) async {
-    try {
-      // Delete pending member document from the pendingMembers collection
-      await pendingMembersCollection.doc(pendingMemberId).delete();
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
     } catch (e) {
@@ -126,37 +77,6 @@ class FireBaseMemberRepo implements MemberRepo {
       return member;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  @override
-  Future<PendingMember> getPendingMemberById(String pendingMemberId) async {
-    try {
-      // Get pending member document from the pendingMembers collection
-      DocumentSnapshot pendingMemberDoc =
-          await pendingMembersCollection.doc(pendingMemberId).get();
-      if (pendingMemberDoc.exists) {
-        return PendingMember.fromJson(
-            pendingMemberDoc.data() as Map<String, dynamic>);
-      } else {
-        throw Exception('Pending member not found');
-      }
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  @override
-  Future<List<PendingMember>> getPendingMembers() async {
-    try {
-      // Get all pending member documents from the pendingMembers collection
-      QuerySnapshot pendingMembersDocs = await pendingMembersCollection.get();
-      return pendingMembersDocs.docs
-          .map((doc) =>
-              PendingMember.fromJson(doc.data() as Map<String, dynamic>))
-          .toList();
     } catch (e) {
       throw Exception(e);
     }
