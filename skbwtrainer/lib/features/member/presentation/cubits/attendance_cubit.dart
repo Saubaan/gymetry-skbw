9e11ef36a4efe573cedf435679af4c893e056e27
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-06 13:02:16 +0530

    Added 2 new methods in attendance_cubit.dart.

--- skbwtrainer/lib/features/member/presentation/cubits/attendance_cubit.dart
+++ skbwtrainer/lib/features/member/presentation/cubits/attendance_cubit.dart
@@ -8,7 +8,31 @@ class AttendanceCubit extends Cubit<AttendanceState> {
 
   AttendanceCubit({required this.memberRepo}) : super(AttendanceInitial());
 
+  // Get all attendance marked today
+  Future<void> getTodayAttendance() async {
+    emit(AttendanceLoading());
+    try {
+      final attendance = await memberRepo.getDayAttendance(DateTime.now());
+      emit(AttendanceLoaded(attendance));
+    } on Exception catch (e) {
+      emit(AttendanceError(e.toString()));
+      List<Attendance> attendance = [];
+      emit(AttendanceLoaded(attendance));
+    }
+  }
 
+  Future<void> getWeekAttendancePerDay() async {
+    emit(AttendanceLoading());
+    try {
+      final attendance = await memberRepo.getWeekAttendancePerDay(DateTime.now());
+      emit(AttendanceWeekLoaded(attendance));
+    } on Exception catch (e) {
+      emit(AttendanceError(e.toString()));
+      List<int> attendance = [0, 0, 0, 0, 0, 0, 0];
+      emit(AttendanceWeekLoaded(attendance));
+
+    }
+  }
 
   Future<void> getAttendance(String id) async {
     emit(AttendanceLoading());
