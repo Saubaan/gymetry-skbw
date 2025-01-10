2216c73c2bd32dd008fab359e64ce1e8935bd824
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-10 21:58:06 +0530

    added authentication.

---
+++ skbwmember/lib/features/auth/data/firebase_auth_repo.dart
@@ -0,0 +1,96 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:skbwmember/features/auth/domain/entities/app_user.dart';
+import 'package:skbwmember/features/auth/domain/entities/pending_member.dart';
+import 'package:skbwmember/features/auth/domain/repositories/auth_repo.dart';
+
+class FirebaseAuthRepo implements AuthRepo {
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  @override
+  Future<AppUser?> loginWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential userCredential =
+          await firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // get user data from firestore
+      DocumentSnapshot userDoc = await FirebaseFirestore.instance
+          .collection('members')
+          .doc(userCredential.user!.uid)
+          .get();
+
+      if (userDoc.exists) {
+        AppUser user = AppUser(
+          uid: userCredential.user!.uid,
+          email: email,
+          name: userDoc['name'],
+        );
+        return user;
+      } else {
+        firebaseAuth.signOut();
+        throw Exception('Member not found');
+      }
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> changePassword(String password, String newPassword) async {
+    try {
+      final user = firebaseAuth.currentUser;
+      if (user != null) {
+        await user.reauthenticateWithCredential(
+          EmailAuthProvider.credential(email: user.email!, password: password),
+        );
+        await user.updatePassword(newPassword);
+      }
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+
+  @override
+  Future<AppUser?> getCurrentUser() async {
+    final firebaseUser = firebaseAuth.currentUser;
+
+    if (firebaseUser == null) {
+      return null;
+    }
+    DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection('members')
+        .doc(firebaseUser.uid)
+        .get();
+
+    if (!userDoc.exists) {
+      return AppUser(
+        uid: firebaseUser.uid,
+        email: firebaseUser.email!,
+        name: userDoc['name'],
+      );
+    } else {
+      firebaseAuth.signOut();
+      return null;
+    }
+  }
+
+  @override
+  Future<void> registerNewMember(PendingMember member) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('pendingMembers')
+          .doc(member.email)
+          .set(member.toJson());
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> logout() async {
+    await firebaseAuth.signOut();
+  }
+}
---
+++ skbwmember/lib/features/auth/presentation/pages/login_page.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skbwmember/theme/app_font.dart';
+
+import '../../../../components/auth_text_field.dart';
+import '../../../../components/primary_button.dart';
+import '../cubits/auth_cubit.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  bool isObscure = true;
+
+  void login() async {
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    final authCubit = context.read<AuthCubit>();
+    if (email.isNotEmpty && password.isNotEmpty) {
+      // Authentication
+      await authCubit.login(email, password);
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sWidth = MediaQuery.of(context).size.width;
+    final sHeight = MediaQuery.of(context).size.height;
+    final theme = Theme.of(context).colorScheme;
+    return Scaffold(
+      backgroundColor: theme.surface,
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Container(
+          height: sHeight,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                theme.secondary.withAlpha(10),
+                theme.secondary.withAlpha(5),
+                theme.primary.withAlpha(-150),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(sWidth / 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(),
+
+                /// Logo Container
+                Column(
+                  children: [
+                    /// Logo
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(sWidth / 40),
+                      child: Image.asset(
+                        'assets/logo/logo-w.png',
+                        height: sWidth / 6,
+                      ),
+                    ),
+
+                    /// Gymetry Trainer Text
+                    Text(
+                      'Gymetry Trainer',
+                      style: TextStyle(
+                        color: theme.onSurface,
+                        fontSize: sWidth / 19,
+                        fontFamily: AppFont.logoFont,
+                      ),
+                    ),
+                  ],
+                ),
+
+                /// Login Form Container
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    /// Login Text
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+
+                        /// Login
+                        Text(
+                          'Login',
+                          style: TextStyle(
+                            color: theme.onSurface,
+                            fontSize: sWidth / 12,
+                            fontFamily: AppFont.primaryFont,
+                          ),
+                        ),
+
+                        /// to your account.
+                        Text(
+                          'to your account.',
+                          style: TextStyle(
+                            color: theme.onSurface.withAlpha(150),
+                            fontSize: sWidth / 18,
+                            fontFamily: AppFont.primaryFont,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: sHeight / 80),
+
+                    /// Login Form
+                    Container(
+                      padding: EdgeInsets.all(sWidth / 40),
+                      decoration: BoxDecoration(
+                          color: theme.onSurface.withAlpha(100),
+                          borderRadius: BorderRadius.circular(sWidth / 20)),
+                      child: Column(
+                        children: [
+                          /// E-mail Field
+                          AuthTextField(
+                            hintText: 'E-mail',
+                            controller: emailController,
+                          ),
+
+                          SizedBox(height: sHeight / 80),
+
+                          /// Password Field
+                          AuthTextField(
+                            hintText: 'Password',
+                            controller: passwordController,
+                            isObscure: isObscure,
+                            onPressed: () {
+                              setState(
+                                    () {
+                                  isObscure = !isObscure;
+                                },
+                              );
+                            },
+                          ),
+
+                          SizedBox(height: sHeight / 80),
+
+                          /// Login Button
+                          PrimaryButton(
+                            text: 'Login',
+                            onTap: login,
+                            color: theme.primary,
+                            textColor: theme.onPrimary,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+
+                /// Gym Name Text
+                Column(
+                  children: [
+                    Text(
+                      'made for',
+                      style: TextStyle(
+                        color: theme.onSurface.withAlpha(100),
+                        fontSize: sWidth / 30,
+                        fontFamily: AppFont.primaryFont,
+                      ),
+                    ),
+                    Text(
+                      'S. K. Body Care',
+                      style: TextStyle(
+                        color: theme.onSurface,
+                        fontSize: sWidth / 24,
+                        fontFamily: AppFont.primaryFont,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
