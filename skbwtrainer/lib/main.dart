085d00f0b45f06a1a125ff298c71c72699d4bf45
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-13 20:55:01 +0530

    Changed imports in main.dart

--- skbwtrainer/lib/main.dart
+++ skbwtrainer/lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'config/firebase_options.dart';
-import 'gymetry.dart';
+import 'package:skbwtrainer/config/firebase_options.dart';
+import 'package:skbwtrainer/gymetry.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
   runApp(Gymetry());
 }
