152b05d8771908a78369cab2321e0f6079e076e7
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-14 20:34:41 +0530

    Created food.dart.

---
+++ skbwmember/lib/features/diet/domain/entities/food.dart
@@ -0,0 +1,11 @@
+class Food {
+  final String name;
+  final String quantity;
+  final String imagePath;
+
+  Food({
+    required this.name,
+    required this.quantity,
+    required this.imagePath,
+  });
+}
