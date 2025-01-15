5e3e731b37be98af0c48f97e876cf6b6e6b949a9
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-15 21:28:55 +0530

    implemented theme_cubit on MaterialApp

--- skbwtrainer/lib/gymetry.dart
+++ skbwtrainer/lib/gymetry.dart
@@ -5,8 +5,7 @@ import 'package:skbwtrainer/features/auth/data/firebase_auth_repo.dart';
 import 'package:skbwtrainer/features/auth/domain/repositories/auth_repo.dart';
 import 'package:skbwtrainer/features/auth/presentation/cubits/auth_states.dart';
 import 'package:skbwtrainer/features/member/presentation/pages/home_page.dart';
-import 'package:skbwtrainer/themes/dark_mode.dart';
-import 'package:skbwtrainer/themes/light_mode.dart';
+import 'package:skbwtrainer/themes/theme_cubit.dart';
 import 'package:skbwtrainer/utils/app_snack_bar.dart';
 import 'package:skbwtrainer/utils/error_message.dart';
 import 'features/auth/presentation/cubits/auth_cubit.dart';
@@ -19,50 +18,62 @@ class Gymetry extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
+        ),
+        
+        BlocProvider(create: (context) => ThemeCubit()..loadTheme()),
+      ],
+      child: BlocBuilder<ThemeCubit, ThemeData>(
+        builder: (context, theme) {
+        return MaterialApp(
+          builder: (context, child) {
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaler: TextScaler.linear(1.0)),
+              child: child!,
+            );
+          },
+          title: 'Gymetry',
+          debugShowCheckedModeBanner: false,
+          theme: theme,
+          home: BlocConsumer<AuthCubit, AuthState>(builder: (context, state) {
+            // Unauthenticated
+            if (state is Unauthenticated) {
+              return const LoginPage();
+            }
 
-    return BlocProvider(
-      create: (context) => AuthCubit(authRepo: authRepo)..checkAuth(),
-      child: MaterialApp(
-        builder: (context, child) {
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(1.0)),
-            child: child!,
-          );
-        },
-        title: 'Gymetry',
-        debugShowCheckedModeBanner: false,
-        theme: lightMode,
-        darkTheme: darkMode,
-        home: BlocConsumer<AuthCubit, AuthState>(
-            builder: (context, state) {
-              // Unauthenticated
-              if (state is Unauthenticated) {
-                return const LoginPage();
-              }
+            // Authenticated
+            else if (state is Authenticated) {
+              return HomePage(
+                user: state.user,
+              );
+            }
 
-              // Authenticated
-              else if (state is Authenticated) {
-                return HomePage(user: state.user,);
-              }
-
-              // Loading
-              else {
-                return Scaffold(
-                  body: Center(
-                    child: LoadingAnimationWidget.staggeredDotsWave(color: Theme.of(context).colorScheme.primary, size: 50),
-                  ),
-                );
-              }
-            },
-            listener: (context, state) {
-              if (state is AuthError) {
-                AppSnackBar.showError(getFirebaseAuthErrorMessage(state.message.replaceAll('Exception:', '')), context);
-              }
-              if (state is AuthSuccess) {
-                AppSnackBar.showSuccess(state.message, context);
-              }
-            }),
-      ),
+            // Loading
+            else {
+              return Scaffold(
+                body: Center(
+                  child: LoadingAnimationWidget.staggeredDotsWave(
+                      color: Theme.of(context).colorScheme.primary, size: 50),
+                ),
+              );
+            }
+          }, listener: (context, state) {
+            if (state is AuthError) {
+              AppSnackBar.showError(
+                  getFirebaseAuthErrorMessage(
+                      state.message.replaceAll('Exception:', '')),
+                  context);
+            }
+            if (state is AuthSuccess) {
+              AppSnackBar.showSuccess(state.message, context);
+            }
+          }),
+        );
+      }),
     );
   }
 }
