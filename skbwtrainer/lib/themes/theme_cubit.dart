2ef1c362dc9c61eb3b856bfab6799c8d4b528741
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-15 21:27:11 +0530

    Created theme_cubit.dart

---
+++ skbwtrainer/lib/themes/theme_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:skbwtrainer/themes/dark_mode.dart';
+import 'package:skbwtrainer/themes/light_mode.dart';
+
+class ThemeCubit extends Cubit<ThemeData> {
+  static const String themeKey = "isDarkTheme";
+
+  ThemeCubit() : super(darkMode);
+
+  Future<void> loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isDark = prefs.getBool(themeKey) ?? true;
+    emit(isDark ? darkMode : lightMode);
+  }
+
+  Future<void> toggleTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    bool isDark = state.brightness == Brightness.dark;
+    emit(isDark ? lightMode : darkMode); // Change theme
+    await prefs.setBool(themeKey, !isDark); // Save new theme preference
+  }
+}
