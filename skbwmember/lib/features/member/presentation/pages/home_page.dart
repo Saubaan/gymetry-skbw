4230b840c0119d47742b1b06b021423f9854c9da
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-16 22:07:41 +0530

    Implemented homepage with state management

--- skbwmember/lib/features/member/presentation/pages/home_page.dart
+++ skbwmember/lib/features/member/presentation/pages/home_page.dart
@@ -1,49 +1,77 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:skbwmember/features/auth/domain/entities/app_user.dart';
-import 'package:skbwmember/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:skbwmember/components/title_card.dart';
+import 'package:skbwmember/features/member/domain/entities/member.dart';
+import 'package:skbwmember/features/member/presentation/components/attendance_calendar_bloc.dart';
+import 'package:skbwmember/features/member/presentation/pages/scanner_page.dart';
+import 'package:skbwmember/theme/app_font.dart';
+import 'package:skbwmember/utils/calendar_functions.dart';
 
 class HomePage extends StatefulWidget {
-  final AppUser user;
-  const HomePage({super.key, required this.user});
+  final Member member;
+  const HomePage({super.key, required this.member});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  void logout() {
-    final authCubit = context.read<AuthCubit>();
-    authCubit.logout();
+  getMemberShipText() {
+    if (widget.member.isPaused) {
+      return 'Membership is paused';
+    } else {
+      if (widget.member.expiryDate.isAfter(DateTime.now())) {
+        return 'Membership active until \n${dateTimeToDate(widget.member.expiryDate)}';
+      } else {
+        return 'Membership expired ${widget.member.expiryDate.difference(DateTime.now()).inDays} days ago';
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-        actions: [
-          IconButton(
-            onPressed: logout,
-            icon: const Icon(Icons.logout),
-          ),
-
-          /// debug button to modify attributes of member documents at once
-          // IconButton(
-          //   onPressed: () {
-          //     final FirebaseFirestore firestore = FirebaseFirestore.instance;
-          //     firestore.collection('members').get().then((snapshot) {
-          //       for (final doc in snapshot.docs) {
-          //          doc.reference.update({'checkDate': Timestamp.now()});;
-          //       }
-          //     });
-          //   },
-          //   icon: const Icon(Icons.add),
-          // )
-        ],
-      ),
-      body: const Center(
-        child: Text('Home Page'),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TitleCard(
+              title: 'Hello there!',
+              childrenAlign: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.member.name,
+                  style:
+                      TextStyle(fontSize: 20, fontFamily: AppFont.primaryFont),
+                ),
+                Text(
+                  getMemberShipText(),
+                  style: TextStyle(
+                    color: widget.member.isPaused
+                        ? Colors.red
+                        : widget.member.expiryDate.isAfter(DateTime.now())
+                            ? Colors.green
+                            : Colors.red,
+                    fontSize: 13,
+                    fontFamily: AppFont.primaryFont,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            TitleCard(
+              title: 'Attendance Calendar',
+              children: [
+                AttendanceCalendarBloc(
+                  startDate: widget.member.createdAt,
+                  endDate: widget.member.expiryDate,
+                  focusedDay: DateTime.now(),
+                  id: widget.member.uid,
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ skbwmember/lib/features/member/presentation/pages/home_page_bloc.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skbwmember/features/auth/domain/entities/app_user.dart';
+import 'package:skbwmember/features/auth/presentation/cubits/auth_cubit.dart';
+import 'package:skbwmember/features/member/data/firebase_member_repo.dart';
+import 'package:skbwmember/features/member/domain/repositories/member_repo.dart';
+import 'package:skbwmember/features/member/presentation/components/app_drawer.dart';
+import 'package:skbwmember/features/member/presentation/cubits/member_cubit.dart';
+import 'package:skbwmember/features/member/presentation/cubits/member_states.dart';
+import 'package:skbwmember/features/member/presentation/pages/home_page.dart';
+import 'package:skbwmember/theme/app_font.dart';
+
+class HomePageBloc extends StatefulWidget {
+  final AppUser user;
+  const HomePageBloc({super.key, required this.user});
+
+  @override
+  State<HomePageBloc> createState() => _HomePageBlocState();
+}
+
+class _HomePageBlocState extends State<HomePageBloc> {
+  final MemberRepo memberRepo = FirebaseMemberRepo();
+  void logout() {
+    final authCubit = context.read<AuthCubit>();
+    authCubit.logout();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context).colorScheme;
+    return BlocProvider(
+      create: (context) =>
+          MemberCubit(memberRepo: memberRepo)..getMember(widget.user.uid),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: theme.primary,
+          foregroundColor: theme.onPrimary,
+          title: Text(
+            'SK Body Care',
+            style: TextStyle(
+              fontSize: 20,
+              fontFamily: AppFont.primaryFont,
+            ),
+          ),
+          actions: [
+            IconButton(
+              onPressed: logout,
+              icon: const Icon(Icons.logout),
+            ),
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: BlocConsumer<MemberCubit, MemberState>(builder: (context, state) {
+          if (state is MemberLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is MemberLoaded) {
+            return HomePage(member: state.member);
+          } else {
+            return const Center(child: Text('No Data Found'));
+          }
+        }, listener: (context, state) {
+          if (state is MemberError) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        }),
+      ),
+    );
+  }
+}
