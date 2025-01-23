b949060b2d49383c99f819b7005adb9e4393fa69
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-23 17:59:54 +0530

    Created exercise_data.dart

---
+++ skbwmember/lib/features/exercises/data/exercise_data.dart
@@ -0,0 +1,10 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> exercises = [
+  Exercise(
+    id: '1',
+    name: 'Lateral Raises',
+    description: 'Lateral raises are a great exercise for building shoulder muscles.',
+    videoPath: 'assets/exercises/1.mp4',
+  ),
+];
