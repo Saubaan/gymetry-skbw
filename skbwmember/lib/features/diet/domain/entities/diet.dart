73e70d23a44737af93c4da4c24014cc16db4227a
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-26 12:35:42 +0530

    Added imagePath attribute

--- skbwmember/lib/features/diet/domain/entities/diet.dart
+++ skbwmember/lib/features/diet/domain/entities/diet.dart
@@ -2,10 +2,12 @@ import 'package:skbwmember/features/diet/domain/entities/time_slot.dart';
 
 class Diet {
   final String name;
+  final String imagePath;
   final List<TimeSlot> timeSlots;
 
   Diet({
     required this.name,
     required this.timeSlots,
+    required this.imagePath,
   });
 }
