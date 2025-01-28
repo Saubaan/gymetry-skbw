5fc559f42e00bb2d99d50ed77e2353b0fcdadeaf
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-28 15:34:04 +0530

    Redesigned profile_page.dart

--- skbwtrainer/lib/features/profile/presentation/pages/profile_page.dart
+++ skbwtrainer/lib/features/profile/presentation/pages/profile_page.dart
@@ -11,66 +11,118 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final sWidth = MediaQuery.of(context).size.width;
-
     final theme = Theme.of(context).colorScheme;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.primary,
-        foregroundColor: theme.onPrimary,
-        centerTitle: true,
-        title: Text(
-          'Profile',
-          style: TextStyle(fontFamily: AppFont.primaryFont),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TitleCard(
-                title: 'Gym Details',
-                childrenAlign: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    gym.name,
-                    style: TextStyle(fontFamily: AppFont.primaryFont),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Phone: ${gym.phone}',
-                    style: TextStyle(fontFamily: AppFont.primaryFont),
-                  ),
-                  SizedBox(height: 10),
-                ],
-              ),
-              SizedBox(height: 10),
-              TitleCard(
-                title: 'Gym QR Code',
-                children: [
-                  Container(
-                    height: sWidth * 0.5,
-                    padding: const EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.white),
-                      borderRadius: BorderRadius.circular(10),
+
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TitleCard(
+              title: 'Gym QR Code',
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        theme.secondary,
+                        theme.primary.withAlpha(50),
+                      ],
                     ),
-                    child: QrImageView(
-                      data: gym.gymCode,
-                      version: QrVersions.auto,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: sWidth*0.1),
+                      Container(
+                        height: sWidth * 0.6,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: QrImageView(
+                          data: gym.gymCode,
+                          version: QrVersions.auto,
+                          eyeStyle: QrEyeStyle(
+                            eyeShape: QrEyeShape.square,
+                            color: Colors.grey.shade900,
+                          ),
+                          dataModuleStyle: QrDataModuleStyle(
+                            dataModuleShape: QrDataModuleShape.square,
+                            color: Colors.grey.shade900,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: sWidth*0.1),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            TitleCard(
+              title: 'Gym Details',
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        theme.secondary,
+                        theme.primary.withAlpha(50),
+                      ],
                     ),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Code: ${gym.gymCode}',
-                    style: TextStyle(fontFamily: AppFont.primaryFont),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.pix_rounded),
+                        title: Text(
+                          gym.name,
+                          style: TextStyle(
+                            color: theme.onSecondary,
+                            fontFamily: AppFont.primaryFont,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Divider(),
+                      ListTile(
+                        leading: Icon(Icons.phone),
+                        title: Text(
+                          gym.phone,
+                          style: TextStyle(
+                            fontFamily: AppFont.primaryFont,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.location_on),
+                        title: Text(
+                          gym.address,
+                          style: TextStyle(
+                            fontFamily: AppFont.primaryFont,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
                   ),
-                  SizedBox(height: 10),
-                ],
-              )
-            ],
-          ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+          ],
         ),
       ),
     );
