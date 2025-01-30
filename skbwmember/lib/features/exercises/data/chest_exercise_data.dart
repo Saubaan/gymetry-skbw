565897c3a97cfde1b28b72619cfe7e929871d7b6
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-30 23:18:19 +0530

    Added Chest Exercises data

---
+++ skbwmember/lib/features/exercises/data/chest_exercise_data.dart
@@ -0,0 +1,108 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> chestExercises = [
+  // flat bench press
+  Exercise(
+    id: '1',
+    name: 'Flat Bench Press',
+    description: 'Flat Bench Press is a great exercise for building strength and '
+        'muscle mass in the chest, shoulders, and triceps. It is a compound '
+        'exercise that targets multiple muscle groups at once.',
+    videoPath: 'assets/exercises/chest/1.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // incline bench press
+  Exercise(
+    id: '2',
+    name: 'Incline Bench Press',
+    description: 'Incline Bench Press is a variation of the Bench Press exercise '
+        'that targets the muscles of the upper chest, shoulders, and triceps. '
+        'It is a great exercise for building strength and muscle mass in the '
+        'upper body.',
+    videoPath: 'assets/exercises/chest/2.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // decline bench press
+  Exercise(
+    id: '3',
+    name: 'Decline Bench Press',
+    description: 'Decline Bench Press is a variation of the Bench Press exercise '
+        'that targets the muscles of the lower chest, shoulders, and triceps. '
+        'It is a great exercise for building strength and muscle mass in the '
+        'upper body.',
+    videoPath: 'assets/exercises/chest/3.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // dumbbell bench press
+  Exercise(
+    id: '4',
+    name: 'Dumbbell Bench Press',
+    description: 'Dumbbell Bench Press is a great exercise for building strength '
+        'and muscle mass in the chest, shoulders, and triceps. It is a compound '
+        'exercise that targets multiple muscle groups at once.',
+    videoPath: 'assets/exercises/chest/4.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // dumbbell fly
+  Exercise(
+    id: '5',
+    name: 'Dumbbell Fly',
+    description: 'Dumbbell Fly is a great exercise for building strength and '
+        'muscle mass in the chest and shoulders. It is a great exercise for '
+        'isolating the chest muscles and building overall upper body strength.',
+    videoPath: 'assets/exercises/chest/5.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // incline dumbbell press
+  Exercise(
+    id: '6',
+    name: 'Incline Dumbbell Press',
+    description: 'Incline Dumbbell Press is a variation of the Dumbbell Bench Press '
+        'exercise that targets the muscles of the upper chest, shoulders, and '
+        'triceps. It is a great exercise for building strength and muscle mass '
+        'in the upper body.',
+    videoPath: 'assets/exercises/chest/6.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // decline dumbbell press
+  Exercise(
+    id: '7',
+    name: 'Decline Dumbbell Press',
+    description: 'Decline Dumbbell Press is a variation of the Dumbbell Bench Press '
+        'exercise that targets the muscles of the lower chest, shoulders, and '
+        'triceps. It is a great exercise for building strength and muscle mass '
+        'in the upper body.',
+    videoPath: 'assets/exercises/chest/7.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // cable fly
+  Exercise(
+    id: '8',
+    name: 'Cable Fly',
+    description: 'Cable Fly is a great exercise for building strength and muscle '
+        'mass in the chest and shoulders. It is a great exercise for isolating '
+        'the chest muscles and building overall upper body strength.',
+    videoPath: 'assets/exercises/chest/8.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // seated chest press
+  Exercise(
+    id: '9',
+    name: 'Seated Chest Press',
+    description: 'Seated Chest Press is a great exercise for building strength and '
+        'muscle mass in the chest, shoulders, and triceps. It is a compound '
+        'exercise that targets multiple muscle groups at once.',
+    videoPath: 'assets/exercises/chest/9.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // pec deck fly
+  Exercise(
+    id: '10',
+    name: 'Pec Deck Fly',
+    description: 'Pec Deck Fly is a great exercise for building strength and muscle '
+        'mass in the chest and shoulders. It is a great exercise for isolating '
+        'the chest muscles and building overall upper body strength.',
+    videoPath: 'assets/exercises/chest/10.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+];
