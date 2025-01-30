19048acfdd1d632e229a929dd6c8b666e329387e
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-30 23:19:18 +0530

    Added back workouts in regular routine

--- skbwmember/lib/features/exercises/data/routine_data.dart
+++ skbwmember/lib/features/exercises/data/routine_data.dart
@@ -1,26 +1,12 @@
-import 'package:skbwmember/features/exercises/data/exercise_data.dart';
-import 'package:skbwmember/features/exercises/domain/entities/routine.dart';
+import 'package:skbwmember/features/exercises/data/back_exercise_data.dart';
 import 'package:skbwmember/features/exercises/domain/entities/workout.dart';
 
-List<Workout> beginnerRoutine = [
+List<Workout> regularRoutine = [
   Workout(
-    id: '1',
-    name: 'Shoulder Workout',
-    description: 'This workout is designed to build shoulder muscles.',
-    imagePath: '',
-    exercises: [
-      exercises[0],
-      exercises[0],
-      exercises[0],
-      exercises[0],
-    ],
-  ),
-];
-
-List<Routine> routines = [
-  Routine(
-    id: '1',
-    name: 'Beginner Routine',
-    workouts: beginnerRoutine,
+    id: 'back',
+    name: 'Back Workouts',
+    description: '',
+    imagePath: 'assets/exercises/icons/back.png',
+    exercises: backExercises,
   ),
 ];
