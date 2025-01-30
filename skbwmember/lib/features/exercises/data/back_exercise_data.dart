34b85fda048e7b260feb160673227084df47a03a
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-30 23:17:58 +0530

    Added Back Exercises data

---
+++ skbwmember/lib/features/exercises/data/back_exercise_data.dart
@@ -0,0 +1,124 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> backExercises = [
+  Exercise(
+    id: '1',
+    name: 'Lat Pulldown',
+    description: 'Lat Pulldown develops the muscles of the back, shoulders, and '
+        'arms. It is a great exercise for building strength and muscle mass in '
+        'the upper body.',
+    videoPath: 'assets/exercises/back/1.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  Exercise(
+    id: '2',
+    name: 'Rear Neck Lat Pulldown',
+    description: 'Rear Neck Lat Pulldown is a variation of the Lat Pulldown '
+        'exercise that targets the muscles of the upper back and shoulders. '
+        'It is a great exercise for building strength and muscle mass in the '
+        'upper body.',
+    videoPath: 'assets/exercises/back/2.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // underhand lat pulldown
+  Exercise(
+    id: '3',
+    name: 'Underhand Lat Pulldown',
+    description: 'Underhand Lat Pulldown is a variation of the Lat Pulldown '
+        'exercise that targets the muscles of the upper back and shoulders. '
+        'It is a great exercise for building strength and muscle mass in the '
+        'upper body.',
+    videoPath: 'assets/exercises/back/3.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // bent over dumbbell row
+  Exercise(
+    id: '4',
+    name: 'Bent Over Dumbbell Row',
+    description: 'Bent Over Dumbbell Row is a great exercise for building '
+        'strength and muscle mass in the upper back and shoulders. It also '
+        'targets the muscles of the arms and core.',
+    videoPath: 'assets/exercises/back/4.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // barbell row
+  Exercise(
+    id: '5',
+    name: 'Barbell Row',
+    description: 'Barbell Row is a great exercise for building strength and '
+        'muscle mass in the upper back and shoulders. It also targets the '
+        'muscles of the arms and core.',
+    videoPath: 'assets/exercises/back/5.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // deadlift
+  Exercise(
+    id: '6',
+    name: 'Deadlift',
+    description: 'Deadlift is a great exercise for building strength and muscle '
+        'mass in the lower back, glutes, hamstrings, and core. It is also a '
+        'great exercise for improving overall strength and power.',
+    videoPath: 'assets/exercises/back/6.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // rear delt fly
+  Exercise(
+    id: '7',
+    name: 'Rear Delt Fly',
+    description: 'Rear Delt Fly is a great exercise for building strength and '
+        'muscle mass in the rear deltoids, upper back, and shoulders. It is '
+        'also a great exercise for improving posture and shoulder health.',
+    videoPath: 'assets/exercises/back/7.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // seated row
+  Exercise(
+    id: '8',
+    name: 'Seated Row',
+    description: 'Seated Row is a great exercise for building strength and '
+        'muscle mass in the upper back, shoulders, and arms. It is also a great '
+        'exercise for improving posture and shoulder health.',
+    videoPath: 'assets/exercises/back/8.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // standing lat pulldown
+  Exercise(
+    id: '9',
+    name: 'Standing Lat Pulldown',
+    description: 'Standing Lat Pulldown is a great exercise for building strength '
+        'and muscle mass in the upper back, shoulders, and arms. It is also a '
+        'great exercise for improving posture and shoulder health.',
+    videoPath: 'assets/exercises/back/9.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // incline t-bar row
+  Exercise(
+    id: '10',
+    name: 'Incline T-Bar Row',
+    description: 'Incline T-Bar Row is a great exercise for building strength and '
+        'muscle mass in the upper back, shoulders, and arms. It is also a great '
+        'exercise for improving posture and shoulder health.',
+    videoPath: 'assets/exercises/back/10.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // cable rope lat pulldown
+  Exercise(
+    id: '11',
+    name: 'Cable Rope Lat Pulldown',
+    description: 'Cable Rope Lat Pulldown is a great exercise for building strength '
+        'and muscle mass in the upper back, shoulders, and arms. It is also a '
+        'great exercise for improving posture and shoulder health.',
+    videoPath: 'assets/exercises/back/11.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // close grip lat pulldown
+  Exercise(
+    id: '12',
+    name: 'Close Grip Lat Pulldown',
+    description: 'Close Grip Lat Pulldown is a great exercise for building strength '
+        'and muscle mass in the upper back, shoulders, and arms. It is also a '
+        'great exercise for improving posture and shoulder health.',
+    videoPath: 'assets/exercises/back/12.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+];
