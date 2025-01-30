ff30a23df6159e842e55233919656a388d8d1238
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-01-30 23:18:07 +0530

    Added Bicep Exercises data

---
+++ skbwmember/lib/features/exercises/data/bicep_exercise_data.dart
@@ -0,0 +1,104 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> bicepExercises = [
+  // barbell curl
+  Exercise(
+    id: '1',
+    name: 'Barbell Curl',
+    description: 'Barbell Curl is a great exercise for building strength and '
+        'muscle mass in the biceps. It also targets the muscles of the forearms '
+        'and shoulders.',
+    videoPath: 'assets/exercises/bicep/1.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // close grip barbell curl
+  Exercise(
+    id: '2',
+    name: 'Close Grip Barbell Curl',
+    description: 'Close Grip Barbell Curl is a variation of the Barbell Curl '
+        'exercise that targets the muscles of the biceps and forearms. It is a '
+        'great exercise for building strength and muscle mass in the upper body.',
+    videoPath: 'assets/exercises/bicep/2.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // dumbbell curl
+  Exercise(
+    id: '3',
+    name: 'Dumbbell Curl',
+    description: 'Dumbbell Curl is a great exercise for building strength and '
+        'muscle mass in the biceps. It also targets the muscles of the forearms '
+        'and shoulders.',
+    videoPath: 'assets/exercises/bicep/3.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // hammer curl
+  Exercise(
+    id: '4',
+    name: 'Hammer Curl',
+    description: 'Hammer Curl is a great exercise for building strength and '
+        'muscle mass in the biceps and forearms. It is a great exercise for '
+        'building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/4.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  //concentration curl
+  Exercise(
+    id: '5',
+    name: 'Concentration Curl',
+    description: 'Concentration Curl is a great exercise for building strength '
+        'and muscle mass in the biceps. It is a great exercise for isolating '
+        'the biceps and building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/5.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // preacher curl
+  Exercise(
+    id: '6',
+    name: 'Preacher Curl',
+    description: 'Preacher Curl is a great exercise for building strength and '
+        'muscle mass in the biceps. It is a great exercise for isolating the '
+        'biceps and building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/6.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // cable curl
+  Exercise(
+    id: '7',
+    name: 'Cable Curl',
+    description: 'Cable Curl is a great exercise for building strength and '
+        'muscle mass in the biceps. It is a great exercise for isolating the '
+        'biceps and building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/7.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // reverse cable curl
+  Exercise(
+    id: '8',
+    name: 'Reverse Cable Curl',
+    description: 'Reverse Cable Curl is a great exercise for building strength '
+        'and muscle mass in the biceps and forearms. It is a great exercise for '
+        'building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/8.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // one arm cable curl
+  Exercise(
+    id: '9',
+    name: 'One Arm Cable Curl',
+    description: 'One Arm Cable Curl is a great exercise for building strength '
+        'and muscle mass in the biceps. It is a great exercise for isolating '
+        'the biceps and building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/9.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // rope hammer curl
+  Exercise(
+    id: '10',
+    name: 'Rope Hammer Curl',
+    description: 'Rope Hammer Curl is a great exercise for building strength '
+        'and muscle mass in the biceps and forearms. It is a great exercise for '
+        'building overall arm strength and size.',
+    videoPath: 'assets/exercises/bicep/10.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+];
