a9b231fde86581ee548a783b3aa580c964bac8c9
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-02-01 22:57:15 +0530

    Added forearm exercise data

---
+++ skbwmember/lib/features/exercises/data/forearm_exercise_data.dart
@@ -0,0 +1,64 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> forearmExercises = [
+  // wrist curl
+  Exercise(
+    id: '1',
+    name: 'Wrist Curl',
+    description: 'Wrist Curl is a great exercise for building strength and '
+        'muscle mass in the forearms. It also targets the muscles of the biceps '
+        'and shoulders.',
+    videoPath: 'assets/exercises/forearm/1.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // reverse wrist curl
+  Exercise(
+    id: '2',
+    name: 'Reverse Wrist Curl',
+    description: 'Reverse Wrist Curl is a variation of the Wrist Curl exercise '
+        'that targets the muscles of the forearms and biceps. It is a great '
+        'exercise for building strength and muscle mass in the upper body.',
+    videoPath: 'assets/exercises/forearm/2.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // standing wrist curl
+  Exercise(
+    id: '3',
+    name: 'Standing Wrist Curl',
+    description: 'Standing Wrist Curl is a great exercise for building strength '
+        'and muscle mass in the forearms. It also targets the muscles of the biceps '
+        'and shoulders.',
+    videoPath: 'assets/exercises/forearm/3.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // wrist rotation
+  Exercise(
+    id: '4',
+    name: 'Wrist Rotation',
+    description: 'Wrist Rotation is a great exercise for building strength and '
+        'muscle mass in the forearms. It also targets the muscles of the biceps '
+        'and shoulders.',
+    videoPath: 'assets/exercises/forearm/4.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // behind the back wrist curl
+  Exercise(
+    id: '5',
+    name: 'Behind The Back Wrist Curl',
+    description: 'Behind The Back Wrist Curl is a great exercise for building '
+        'strength and muscle mass in the forearms. It also targets the muscles '
+        'of the biceps and shoulders.',
+    videoPath: 'assets/exercises/forearm/5.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // barbell wrist curl
+  Exercise(
+    id: '6',
+    name: 'Barbell Wrist Curl',
+    description: 'Barbell Wrist Curl is a great exercise for building strength '
+        'and muscle mass in the forearms. It also targets the muscles of the biceps '
+        'and shoulders.',
+    videoPath: 'assets/exercises/forearm/6.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+];
