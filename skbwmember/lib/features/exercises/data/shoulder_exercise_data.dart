823118e11d14fd80cb86050b4f115339f6caba4b
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-02-01 22:57:00 +0530

    Added shoulder exercise data

---
+++ skbwmember/lib/features/exercises/data/shoulder_exercise_data.dart
@@ -0,0 +1,93 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> shoulderExercises = [
+  // shoulder press
+  Exercise(
+    id: '1',
+    name: 'Shoulder Press',
+    description: 'Shoulder Press is a great exercise for building strength and '
+        'muscle mass in the shoulders. It also targets the muscles of the arms '
+        'and core.',
+    videoPath: 'assets/exercises/shoulder/1.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // back shoulder press
+  Exercise(
+    id: '2',
+    name: 'Back Shoulder Press',
+    description: 'Back Shoulder Press is a variation of the Shoulder Press '
+        'exercise that targets the muscles of the upper back and shoulders. '
+        'It is a great exercise for building strength and muscle mass in the '
+        'upper body.',
+    videoPath: 'assets/exercises/shoulder/2.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // dumbbell shoulder press
+  Exercise(
+    id: '3',
+    name: 'Dumbbell Shoulder Press',
+    description: 'Dumbbell Shoulder Press is a great exercise for building '
+        'strength and muscle mass in the shoulders. It also targets the muscles '
+        'of the arms and core.',
+    videoPath: 'assets/exercises/shoulder/3.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // lateral raise
+  Exercise(
+    id: '4',
+    name: 'Lateral Raise',
+    description: 'Lateral Raise is a great exercise for building strength and '
+        'muscle mass in the shoulders. It also targets the muscles of the arms '
+        'and core.',
+    videoPath: 'assets/exercises/shoulder/4.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // front raise
+  Exercise(
+    id: '5',
+    name: 'Front Raise',
+    description: 'Front Raise is a great exercise for building strength and '
+        'muscle mass in the shoulders. It also targets the muscles of the arms '
+        'and core.',
+    videoPath: 'assets/exercises/shoulder/5.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // Lateral machine raise
+  Exercise(
+    id: '6',
+    name: 'Lateral Machine Raise',
+    description: 'Lateral Machine Raise is a great exercise for building strength '
+        'and muscle mass in the shoulders. It also targets the muscles of the arms '
+        'and core.',
+    videoPath: 'assets/exercises/shoulder/6.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // cable front raise
+  Exercise(
+    id: '7',
+    name: 'Cable Front Raise',
+    description: 'Cable Front Raise is a great exercise for building strength and '
+        'muscle mass in the shoulders. It also targets the muscles of the arms and '
+        'core.',
+    videoPath: 'assets/exercises/shoulder/7.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // upright row
+  Exercise(
+    id: '8',
+    name: 'Upright Row',
+    description: 'Upright Row is a great exercise for building strength and muscle '
+        'mass in the shoulders. It also targets the muscles of the arms and core.',
+    videoPath: 'assets/exercises/shoulder/8.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // shrugs
+  Exercise(
+    id: '9',
+    name: 'Shrugs',
+    description: 'Shrugs is a great exercise for building strength and muscle mass '
+        'in the shoulders. It also targets the muscles of the arms and core.',
+    videoPath: 'assets/exercises/shoulder/9.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+];
