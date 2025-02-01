c2446b19bf96646904594d765a755aa7db59419f
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-02-01 22:56:27 +0530

    Removed unnecessary attribute

--- skbwmember/lib/features/exercises/domain/entities/workout.dart
+++ skbwmember/lib/features/exercises/domain/entities/workout.dart
@@ -3,14 +3,12 @@ import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
 class Workout {
   final String id;
   final String name;
-  final String description;
   final String imagePath;
   final List<Exercise> exercises;
 
   Workout({
     required this.id,
     required this.name,
-    required this.description,
     required this.imagePath,
     required this.exercises,
   });
