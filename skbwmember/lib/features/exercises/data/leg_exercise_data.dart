f67fed1b601bbb6c0d7ccbb989d7bb6f74b533f1
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-02-01 22:57:26 +0530

    Added leg exercise data

---
+++ skbwmember/lib/features/exercises/data/leg_exercise_data.dart
@@ -0,0 +1,84 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> legExercises = [
+  // squats
+  Exercise(
+    id: '1',
+    name: 'Squats',
+    description: 'Squats is a great exercise for building strength and muscle '
+        'mass in the legs, glutes, and core. It is also a great exercise for '
+        'improving overall strength and power.',
+    videoPath: 'assets/exercises/leg/1.mp4',
+    credit: 'Umar Jakatdar',
+  ),
+  // dumbbell squats
+  Exercise(
+    id: '2',
+    name: 'Dumbbell Squats',
+    description: 'Dumbbell Squats is a great exercise for building strength and '
+        'muscle mass in the legs, glutes, and core. It is also a great exercise '
+        'for improving overall strength and power.',
+    videoPath: 'assets/exercises/leg/2.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // dumbbell calf raise
+  Exercise(
+    id: '3',
+    name: 'Dumbbell Calf Raise',
+    description: 'Dumbbell Calf Raise is a great exercise for building strength '
+        'and muscle mass in the calves. It is also a great exercise for improving '
+        'overall lower body strength and power.',
+    videoPath: 'assets/exercises/leg/3.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // sumo squats
+  Exercise(
+    id: '4',
+    name: 'Sumo Squats',
+    description: 'Sumo Squats is a great exercise for building strength and muscle '
+        'mass in the legs, glutes, and core. It is also a great exercise for '
+        'improving overall lower body strength and power.',
+    videoPath: 'assets/exercises/leg/4.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // seated calf raise
+  Exercise(
+    id: '5',
+    name: 'Seated Calf Raise',
+    description: 'Seated Calf Raise is a great exercise for building strength and '
+        'muscle mass in the calves. It is also a great exercise for improving '
+        'overall lower body strength and power.',
+    videoPath: 'assets/exercises/leg/5.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // leg press
+  Exercise(
+    id: '6',
+    name: 'Leg Press',
+    description: 'Leg Press is a great exercise for building strength and muscle '
+        'mass in the legs, glutes, and core. It is also a great exercise for '
+        'improving overall lower body strength and power.',
+    videoPath: 'assets/exercises/leg/6.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // leg extension
+  Exercise(
+    id: '7',
+    name: 'Leg Extension',
+    description: 'Leg Extension is a great exercise for building strength and '
+        'muscle mass in the legs, glutes, and core. It is also a great exercise '
+        'for improving overall lower body strength and power.',
+    videoPath: 'assets/exercises/leg/7.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // leg curl
+  Exercise(
+    id: '8',
+    name: 'Leg Curl',
+    description: 'Leg Curl is a great exercise for building strength and muscle '
+        'mass in the legs, glutes, and core. It is also a great exercise for '
+        'improving overall lower body strength and power.',
+    videoPath: 'assets/exercises/leg/8.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+];
