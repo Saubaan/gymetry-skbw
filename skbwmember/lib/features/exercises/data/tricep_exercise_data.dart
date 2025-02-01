ebcbfa74475319f1628fbac5a72fa5565db1c7e5
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-02-01 22:56:47 +0530

    Added tricep exercise data

---
+++ skbwmember/lib/features/exercises/data/tricep_exercise_data.dart
@@ -0,0 +1,94 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> tricepExercises = [
+  // barbell skull crusher
+  Exercise(
+    id: '1',
+    name: 'Barbell Skull Crusher',
+    description: 'Barbell Skull Crusher is a great exercise for building strength '
+        'and muscle mass in the triceps. It also targets the muscles of the '
+        'shoulders and chest.',
+    videoPath: 'assets/exercises/tricep/1.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // dumbbell tricep extension
+  Exercise(
+    id: '2',
+    name: 'Dumbbell Tricep Extension',
+    description: 'Dumbbell Tricep Extension is a great exercise for building '
+        'strength and muscle mass in the triceps. It also targets the muscles '
+        'of the shoulders and chest.',
+    videoPath: 'assets/exercises/tricep/2.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // overhead tricep extension
+  Exercise(
+    id: '3',
+    name: 'Overhead Tricep Extension',
+    description: 'Overhead Tricep Extension is a great exercise for building '
+        'strength and muscle mass in the triceps. It also targets the muscles '
+        'of the shoulders and chest.',
+    videoPath: 'assets/exercises/tricep/3.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // tricep kickback
+  Exercise(
+    id: '4',
+    name: 'Tricep Kickback',
+    description: 'Tricep Kickback is a great exercise for building strength and '
+        'muscle mass in the triceps. It also targets the muscles of the shoulders '
+        'and chest.',
+    videoPath: 'assets/exercises/tricep/4.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // rope tricep pushdown
+  Exercise(
+    id: '5',
+    name: 'Rope Tricep Pushdown',
+    description: 'Rope Tricep Pushdown is a great exercise for building strength '
+        'and muscle mass in the triceps. It also targets the muscles of the '
+        'shoulders and chest.',
+    videoPath: 'assets/exercises/tricep/5.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // tricep pushdown
+  Exercise(
+    id: '6',
+    name: 'Tricep Pushdown',
+    description: 'Tricep Pushdown is a great exercise for building strength and '
+        'muscle mass in the triceps. It also targets the muscles of the shoulders '
+        'and chest.',
+    videoPath: 'assets/exercises/tricep/6.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // reverse grip pushdown
+  Exercise(
+    id: '7',
+    name: 'Reverse Grip Pushdown',
+    description: 'Reverse Grip Pushdown is a great exercise for building strength '
+        'and muscle mass in the triceps. It also targets the muscles of the '
+        'shoulders and chest.',
+    videoPath: 'assets/exercises/tricep/7.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // overhead cable extension
+  Exercise(
+    id: '8',
+    name: 'Overhead Cable Extension',
+    description: 'Overhead Cable Extension is a great exercise for building '
+        'strength and muscle mass in the triceps. It also targets the muscles '
+        'of the shoulders and chest.',
+    videoPath: 'assets/exercises/tricep/8.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+  // seated tricep dip
+  Exercise(
+    id: '9',
+    name: 'Seated Tricep Dip',
+    description: 'Seated Tricep Dip is a great exercise for building strength and '
+        'muscle mass in the triceps. It also targets the muscles of the shoulders '
+        'and chest.',
+    videoPath: 'assets/exercises/tricep/9.mp4',
+    credit: 'Shoaib Shaikh',
+  ),
+];
