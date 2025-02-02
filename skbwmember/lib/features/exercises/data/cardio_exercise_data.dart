3a53086f1d25167cc4d822e2788787c8f6d4fc5a
Saubaan Shaikh <saubaanmshaikh@gmail.com>
2025-02-02 20:35:10 +0530

    Added Cardio exercise data

---
+++ skbwmember/lib/features/exercises/data/cardio_exercise_data.dart
@@ -0,0 +1,14 @@
+import 'package:skbwmember/features/exercises/domain/entities/exercise.dart';
+
+List<Exercise> cardioExercises = [
+  Exercise(
+    id: '1',
+    name: 'Abs Workout',
+    description: 'Abs Workout to burn belly fat and get a flat stomach. These '
+        'exercises will help you to strengthen your core and tone your abs.'
+        '\nNote: The above video contains multiple exercises to target your '
+        'abs. You can pause the video and do the exercises at your own pace.',
+    videoPath: 'assets/exercises/cardio/1.mp4',
+    credit: 'Shoaib Shaikh',
+  )
+];
